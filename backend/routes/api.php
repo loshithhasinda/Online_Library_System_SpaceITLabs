9b21070e0470e9231c7b693813be707a710b1ee9
Loshith Hasinda Samararathna <it20127046@my.sliit.lk>
2022-08-30 15:23:34 +0530

    create book model and controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/auth/register', [AuthController::class, 'createUser']);
 Route::post('/auth/login', [AuthController::class, 'loginUser']);
+
+Route::apiResource('books', BookController::class);
